22a985f2ce5f4c1cbe2b354d3bfc1dc3bef0c48a
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-09-27 21:11:43 -0400

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,21 @@
 import requests
 import json
 
-class GetRequester:
 
-    def __init__(self, url):
+class GetRequester:
+    def __init__(self, url: str):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self) -> str:
+        response = requests.get(self.url)
+        return response.content
 
-    def load_json(self):
-        pass
+    def load_json(self) -> dict:
+        return json.loads(self.get_response_body())
+
+
+if __name__ == "__main__":
+    instance = GetRequester(
+        "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    )
+    print(instance.load_json())
